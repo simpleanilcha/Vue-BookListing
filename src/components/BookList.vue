3af1439908b7640bdac842a9996b57a298398d28
anil.maharjan@chuchuro.com <simpleanilcha@gmail.com>
2020-11-23 12:47:48 +0545

    BookList component created.

---
+++ src/components/BookList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'BookList',
+        data() {
+            return {
+                title: 'All Books'
+            }
+        }
+    }
+</script>
+
+<style>
+h1 {
+    font-weight: normal;
+}
+</style>
